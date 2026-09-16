6ae035113b6ff8f1f53ce74133607db1bf5c32df
Your Name <mohammadmouhyeldeen@gmail.com>
2026-09-16 14:56:26 +0300

    Standardize landing page path to templates/landing/landing.html

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='landing/home.html'), name='home'),
+    path('', TemplateView.as_view(template_name="landing/landing.html"), name="home"),
 ]
